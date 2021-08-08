2e9937ae792a5e09261e5df266af760f2a6318e6
Paulo Barbosa <paulomiguelbarbosa@gmail.com>
2021-08-08 10:44:26 +0000

    removed solution

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 // ref: https://www.codewars.com/kata/525481903700c1a1ff0000e1/train/javascript
 
 function cache(func) {
-  var calls = {};
-  return function() {
-    var key = JSON.stringify(arguments);
-    if (!(key in calls)) {
-      calls[key] = func.apply(null, arguments);
-    }
-    return calls[key];
-  };
+  
 }
 
 module.exports = cache;
